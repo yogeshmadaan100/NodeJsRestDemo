9732330e10913bedcb249bbb79f1a262843f8870
Yogesh <yogeshmadaan100@gmail.com>
2016-04-10 21:21:49 +0530

    Api authentication using jwt

--- server.js
+++ server.js
@@ -2,38 +2,75 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');        // call express
+var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test',function(err){
-    if(err)
-        console.log("connection error"+err);
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var User = require('./models/user'); // get our mongoose model
+var Bear = require('./models/bear');
+
+mongoose.connect('mongodb://127.0.0.1:27017/test', function (err) {
+    if (err)
+        console.log("connection error" + err);
 }); // connect to our database
-var Bear     = require('./models/bear');
 
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
-
+app.set('superSecret', "abc");
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// use morgan to log requests to the console
+app.use(morgan('dev'));
 // middleware to use for all requests
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
     // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
+    console.log(req.url);
+    //console.log(res);
+    if(req.url.toString().indexOf("authenticate")!=-1)
+        next();
+    else{
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
+    }
+})
+;
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+router.get('/', function (req, res) {
+    res.status(400).send({message: 'hooray! welcome to our api!'});
 });
 
 // more routes for our API will happen here
@@ -42,50 +79,49 @@ router.get('/', function(req, res) {
 router.route('/bears')
 
     // create a bear (accessed at POST http://localhost:8080/api/bears)
-    .post(function(req, res) {
+    .post(function (req, res) {
 
         var bear = new Bear();      // create a new instance of the Bear model
         bear.name = req.body.name;  // set the bears name (comes from the request)
         console.log("got request");
         console.log(req.body.name);
         // save the bear and check for errors
-        bear.save(function(err) {
-            if (err)
-            {
+        bear.save(function (err) {
+            if (err) {
                 res.send(err);
                 console.log(err);
             }
 
-            res.json({ message: 'Bear created!' });
+            res.json({message: 'Bear created!'});
         });
 
     })
-    .get(function (req,res) {
-        Bear.find(function(err, bears) {
+    .get(function (req, res) {
+        Bear.find(function (err, bears) {
             if (err)
                 res.send(err);
 
             res.json(bears);
         });
-        
+
     })
 ;
 // on routes that end in /bears/:bear_id
 router.route('/bears/:bear_id')
 
     // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-    .get(function(req, res) {
-        Bear.findById(req.params.bear_id, function(err, bear) {
+    .get(function (req, res) {
+        Bear.findById(req.params.bear_id, function (err, bear) {
             if (err)
                 res.send(err);
             res.json(bear);
         });
     })
     // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-    .put(function(req, res) {
+    .put(function (req, res) {
 
         // use our bear model to find the bear we want
-        Bear.findById(req.params.bear_id, function(err, bear) {
+        Bear.findById(req.params.bear_id, function (err, bear) {
 
             if (err)
                 res.send(err);
@@ -93,25 +129,85 @@ router.route('/bears/:bear_id')
             bear.name = req.body.name;  // update the bears info
 
             // save the bear
-            bear.save(function(err) {
+            bear.save(function (err) {
                 if (err)
                     res.send(err);
 
-                res.json({ message: 'Bear updated!' });
+                res.json({message: 'Bear updated!'});
             });
 
         });
     })// delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
-    .delete(function(req, res) {
+    .delete(function (req, res) {
         Bear.remove({
             _id: req.params.bear_id
-        }, function(err, bear) {
+        }, function (err, bear) {
             if (err)
                 res.send(err);
 
-            res.json({ message: 'Successfully deleted' });
+            res.json({message: 'Successfully deleted'});
         });
     });
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+router.post('/authenticate', function (req, res) {
+
+    // find the user
+    User.findOne({
+        name: req.body.name
+    }, function (err, user) {
+
+        if (err) throw err;
+
+        if (!user) {
+            //res.json({success: false, message: 'Authentication failed. User not found.'});
+            var user = new User();
+            user.name = req.body.name;
+            user.password = req.body.password;
+            user.admin = req.body.admin;
+            // save the bear and check for errors
+            user.save(function (err) {
+                if (err) {
+                    res.statusCode(400).send(err);
+                    console.log(err);
+                }
+
+                var token = jwt.sign(user, app.get('superSecret'), {
+                    expiresIn: 1440 * 60 // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            });
+        } else if (user) {
+
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({success: false, message: 'Authentication failed. Wrong password.'});
+            } else {
+
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign(user, app.get('superSecret'), {
+                    expiresInMinutes: 1440 // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+
+        }
+
+    });
+});
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
